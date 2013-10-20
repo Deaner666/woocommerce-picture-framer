208c4550293991441937a80839576d7d2ec49f6a
Dave Dean <dave@moortor-design.co.uk>
2013-10-20 18:19:28 +0100

    Added plugin header.

---
+++ woocommerce-picture-framer.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Woocommerce Picture Framer
+ * Plugin URI: https://github.com/Deaner666/woocommerce-picture-framer
+ * Description: Plugin for automatically adding picture frame and mount designs to Woocommerce product images and thumbnails.
+ * Version: 0.1
+ * Author: Dave Dean
+ * Author URI: http://www.moortor-design.co.uk
+ * License: GPL2
+ */
+?>
