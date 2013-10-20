71be8f3c194c3a7969209b543287411dfbd46fe9
Dave Dean <dave@moortor-design.co.uk>
2013-10-20 18:21:34 +0100

    Added GPL 2 notice.

--- woocommerce-picture-framer.php
+++ woocommerce-picture-framer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Woocommerce Picture Framer
  * Plugin URI: https://github.com/Deaner666/woocommerce-picture-framer
@@ -8,4 +9,23 @@
  * Author URI: http://www.moortor-design.co.uk
  * License: GPL2
  */
+
+/*  Copyright 2013  Dave Dean  (email : dave@moortor-design.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+
 ?>
