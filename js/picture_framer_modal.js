42c510197392f037ee2840e8d90a0857c3caef24
Dave Dean <dave@moortor-design.co.uk>
2013-10-27 09:33:33 +0000

    Added animation to modal.

--- js/picture_framer_modal.js
+++ js/picture_framer_modal.js
@@ -8,6 +8,8 @@ jQuery(document).ready( function() {
     height: 600,
     draggable: false,
     resizable: false,
+    show: 175,
+    hide: 175,
     buttons: [
       { text: "Ok", click: function() { jQuery( this ).dialog( "close" ); } },
       { text: "Cancel", click: function() { jQuery( this ).dialog( "close" ); } }
