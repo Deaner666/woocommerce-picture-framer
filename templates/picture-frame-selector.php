c9f5d92ec8379ac9e6033de231adccec8e810f6b
Dave Dean <dave@moortor-design.co.uk>
2013-10-28 23:36:52 +0000

    Trying to show frames and mounts separately in modal.

--- templates/picture-frame-selector.php
+++ templates/picture-frame-selector.php
@@ -6,5 +6,57 @@
 	<div id="picture-frame-selector-modal">
 		<h1>You did it!</h1>
 		<p>Here we are - Picture framer!</p>
+
+		<ul class="picture-frames-list">
+		<?php
+			// Picture Frames & Mounts Query
+			// $args = 'post_type' => 'picture_frames';
+			$args = array (
+				'post_type' => 'picture_frames',
+				'meta_query' => array(
+					array(
+						'key' => 'wpf_picture_frame_type',
+						'value' => 'frame',
+						'compare' => '='
+					),
+				)
+			);
+			$picture_frames = new WP_Query( $args );
+
+			// The Picture Frames Loop
+			while ( $picture_frames->have_posts() ) {
+				$picture_frames->the_post();
+				if ( get_post_meta( get_post_ID(), 'wpf_picture_frame_type', true ) == 'frame' ) {
+					echo '<li>' . get_the_title() . '</li>';
+				}
+			}
+			wp_reset_postdata();
+		?>
+		</ul>
+		<ul class="mounts-list">
+		<?php
+			// The Mounts Query
+			$args2 = array (
+				'post_type' => 'picture_frames',
+				'meta_query' => array(
+					array(
+						'key' => 'wpf_picture_frame_type',
+						'value' => 'mount',
+						'compare' => '='
+					),
+				)
+			);
+			$mounts = new WP_Query( $args2 );
+
+			// The Mounts Loop
+			while( $mounts->have_posts() ) {
+				$mounts->the_post();
+				if ( get_post_meta( get_post_ID(), 'wpf_picture_frame_type', true ) == 'mount' ) {
+					echo '<li>' . get_the_title() . '</li>';
+				}
+			}
+			wp_reset_postdata();
+		?>
+		</ul>
 	</div>
 </div>
