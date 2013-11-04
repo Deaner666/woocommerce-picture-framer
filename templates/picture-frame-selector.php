46edfdc6065e2b15b2ca70e583b1ddbeb9fba55e
Dave Dean <dave@moortor-design.co.uk>
2013-11-04 11:24:21 +0000

    Finally got the modal to correctly query for mounts and picture frames.

--- templates/picture-frame-selector.php
+++ templates/picture-frame-selector.php
@@ -7,57 +7,45 @@
 		<h1>You did it!</h1>
 		<p>Here we are - Picture framer!</p>
 
-		<ul class="picture-frames-list">
+		<h2>Picture Frames</h2>
+		<ul id="picture-frames-list">
 		<?php
-			// Picture Frames & Mounts Query
-			$args = array(
-				'post_type' => 'picture_frames',
-				'meta_query' => array(
-					array(
-						'key' => 'wpf_picture_frame_type',
-						'value' => 'frame',
-						'type' => 'CHAR',
-						'compare' => '='
-					)
-				)
-			);
-			$picture_frames = new WP_Query( $args );
-			// $picture_frames = new WP_Query( 'post_type=picture_frames' );
-
-			// The Picture Frames Loop
-			while ( $picture_frames->have_posts() ) {
-				$picture_frames->the_post();
-				if ( get_post_meta( get_post_ID(), 'wpf_picture_frame_type', true ) == 'frame' ) {
-					echo '<li>' . get_the_title() . '</li>';
+			$picture_frames = new WP_Query( 'post_type=picture_frames' );
+			if( $picture_frames->have_posts() ) {
+				while( $picture_frames->have_posts() ) {
+					$picture_frames->the_post();
+					$post_id = get_the_ID();
+					$picture_frame_type = json_decode( get_post_meta($post_id, 'wpf_picture_frame_type', true) );
+					
+					if ( $picture_frame_type == 'frame' ) {
+						echo "<li>" . get_post_meta($post_id, 'wpf_picture_frame_type', true) . "</li>";
+						echo "<li>" . get_the_title() . "</li>";
+					}
 				}
 			}
-			wp_reset_postdata();
+			else {
+				echo 'Uh oh, no picture frames!';
+			}
 		?>
 		</ul>
-		<ul class="mounts-list">
+		<h2>Mounts</h2>
+		<ul id="mounts-list">
 		<?php
-			// The Mounts Query
-			$args2 = array(
-				'post_type' => 'picture_frames',
-				'meta_query' => array(
-					array(
-						'key' => 'wpf_picture_frame_type',
-						'value' => 'mount',
-						'type' => 'CHAR',
-						'compare' => '='
-					)
-				)
-			);
-			$mounts = new WP_Query( $args2 );
-
-			// The Mounts Loop
-			while( $mounts->have_posts() ) {
-				$mounts->the_post();
-				if ( get_post_meta( get_post_ID(), 'wpf_picture_frame_type', true ) == 'mount' ) {
-					echo '<li>' . get_the_title() . '</li>';
+			if( $picture_frames->have_posts() ) {
+				while( $picture_frames->have_posts() ) {
+					$picture_frames->the_post();
+					$post_id = get_the_ID();
+					$picture_frame_type = json_decode( get_post_meta($post_id, 'wpf_picture_frame_type', true) );
+					
+					if ( $picture_frame_type == 'mount' ) {
+						echo "<li>" . get_post_meta($post_id, 'wpf_picture_frame_type', true) . "</li>";
+						echo "<li>" . get_the_title() . "</li>";
+					}
 				}
 			}
-			wp_reset_postdata();
+			else {
+				echo 'Uh oh, no mounts!';
+			}
 		?>
 		</ul>
 	</div>
