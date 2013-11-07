46c7dbabbac1219a8a0249c784862918b6937ad7
Dave Dean <dave@moortor-design.co.uk>
2013-11-07 13:41:04 +0000

    Pulled WooCommerce product image into the modal view.

--- templates/picture-frame-selector.php
+++ templates/picture-frame-selector.php
@@ -1,11 +1,39 @@
-<div id="picture-frame-selector">
+<?php
+	if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+	global $post, $woocommerce, $product;
+?>
+
+<section id="picture-frame-selector">
+	
 	<div id="picture-frame-selector-lead-in">
 		<h3>Picture frames gallery</h3>
 		<p><button class="modal-opener">Click here</button> to try your chosen map with different frames and mounts</p>
 	</div>
-	<div id="picture-frame-selector-modal">
-		<h1>You did it!</h1>
-		<p>Here we are - Picture framer!</p>
+
+	<article id="picture-frame-selector-modal">
+		<header>
+			<h1>Choose your frame and mount</h1>
+		</header>
+
+		<!-- Product thumbnail here -->
+		<?php
+			if ( has_post_thumbnail() ) {
+
+				$image_title 		= esc_attr( get_the_title( get_post_thumbnail_id() ) );
+				$image_link  		= wp_get_attachment_url( get_post_thumbnail_id() );
+				$image       		= get_the_post_thumbnail( $post->ID, apply_filters( 'single_product_large_thumbnail_size', 'shop_single' ), array(
+					'title' => $image_title
+					) );
+
+				echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<a href="%s" itemprop="image" class="woocommerce-main-image zoom" title="%s"  rel="prettyPhoto' . $gallery . '">%s</a>', $image_link, $image_title, $image ), $post->ID );
+
+			} else {
+
+				echo apply_filters( 'woocommerce_single_product_image_html', sprintf( '<img src="%s" alt="Placeholder" class="woocommerce-main-image zoom"/>', woocommerce_placeholder_img_src() ), $post->ID );
+
+			}
+		?>
 
 		<h2>Picture Frames</h2>
 		<ul id="picture-frames-list">
@@ -34,6 +62,7 @@
 			}
 		?>
 		</ul>
+
 		<h2>Mounts</h2>
 		<ul id="mounts-list">
 		<?php
@@ -60,5 +89,6 @@
 			}
 		?>
 		</ul>
-	</div>
-</div>
+	</article>
+
+</section>
